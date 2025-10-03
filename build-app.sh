65c77b223e018c4d8f199266386693ea33f40816
MaviR04 <mavirodrigo4@gmail.com>
2025-10-03 13:29:39 +0530

    deploy change

---
+++ build-app.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Make sure this file has executable permissions, run `chmod +x build-app.sh`
+
+# Exit the script if any command fails
+set -e
+
+
+# Clear cache
+php artisan optimize:clear
+
+# Cache the various components of the Laravel application
+php artisan config:cache
+php artisan event:cache
+php artisan route:cache
+php artisan view:cache
+
+chmod +x run-worker.sh
